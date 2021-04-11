818f5b3d8e6de60532141533eb436ffa13a55700
maxtheaxe <max.perrello@gmail.com>
2021-04-11 16:52:46 -0400

    updated server to accept arguments properly

--- trailer_server.py
+++ trailer_server.py
@@ -2,17 +2,18 @@
 
 import uvicorn
 from fastapi import FastAPI
+import trailer_scraper
+import asyncio
 
 app = FastAPI()
 
 
-# @app.get("/")
-# def root():
-#     return {"message": "Hello World"}
-
 @app.get("/")
-def get_preview(movie_title: str = "Batman Begins"):
-    return {"trailer_link": "https://www.imdb.com/video/vi2992219161"}
+async def get_preview(source: str, movie_title: str):
+	driver = trailer_scraper.start_driver()
+	video_list = trailer_scraper.search_movie(driver, source, movie_title)
+	return video_list
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+	asyncio.run(uvicorn.run(app, host="0.0.0.0", port=8000))
