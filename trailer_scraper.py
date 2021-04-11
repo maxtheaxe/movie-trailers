79b992ab1ecb93c1bd575ca8c63e484cd04b117a
maxtheaxe <max.perrello@gmail.com>
2021-04-11 17:22:59 -0400

    added basic imdb scraping

--- trailer_scraper.py
+++ trailer_scraper.py
@@ -9,7 +9,8 @@ import chromedriver_autoinstaller as cda  # handle chromedriver for new installs
 from bs4 import BeautifulSoup
 import time
 
-def start_driver(headless: bool = False) -> webdriver.Chrome:
+
+def start_driver(headless: bool = True) -> webdriver.Chrome:
 	'''
 	Starts the webdriver (dls if needed) and returns it.
 	
@@ -27,7 +28,8 @@ def start_driver(headless: bool = False) -> webdriver.Chrome:
 	# other settings
 	options.headless = headless  # headless or not, passed as arg (true by default)
 	options.add_experimental_option('excludeSwitches', ['enable-logging'])  # clean up console
-	return webdriver.Chrome(options = options) # return set up driver
+	return webdriver.Chrome(options=options)  # return set up driver
+
 
 def youtube_search(driver: webdriver.Chrome, movie_title: str, num_results: int = 100) -> list:
 	'''
@@ -40,34 +42,89 @@ def youtube_search(driver: webdriver.Chrome, movie_title: str, num_results: int 
 	Returns:
 		list
 	'''
-	driver.get("https://www.youtube.com/") # load page
-	driver.find_element_by_id("search").send_keys(movie_title) # type search string
-	driver.find_element_by_id("search-icon-legacy").click() # click search button
+	driver.get("https://www.youtube.com/")  # load page
+	driver.find_element_by_id("search").send_keys(movie_title)  # type search string
+	driver.find_element_by_id("search-icon-legacy").click()  # click search button
 	# load appropriate number of results (keep scrolling while num results < desired)
 	while len(driver.find_elements_by_xpath(
 			"//ytd-video-renderer//a[@id='video-title']")) < num_results:
-		driver.execute_script("window.scrollBy(0,300)", "") # scroll down w JS
+		driver.execute_script("window.scrollBy(0,300)", "")  # scroll down w JS
 	# save all results once desired number loaded (at least partially)
 	results = driver.find_elements_by_xpath("//ytd-video-renderer//a[@id='video-title']")
 	# wait for the last element to load fully
-	try:  # loook for filters menu as indicator of load (we know it will always be there)
-		wait = WebDriverWait(driver, 10) # wait a maximum of 10 seconds for load
+	try:  # look for filters menu as indicator of load (we know it will always be there)
+		wait = WebDriverWait(driver, 10)  # wait a maximum of 10 seconds for load
 		# feature used below doesn't exist yet, but I've implemented it my local version
 		# and submitted a pull request to the main repo on github:
 		# https://github.com/SeleniumHQ/selenium/pull/9374
-		element = wait.until(EC.element_to_be_clickable(results[-1])) # target last video
+		element = wait.until(EC.element_to_be_clickable(results[-1]))  # target last video
 	except NoSuchElementException as Error:  # page didn't load in a reasonable amount of time
 		print("\n\tError: Page didn't load in a reasonable amount of time; try again.\n")
 		print("\n\t{}\n".format(str(Error)))
 	# grab the source of the page and parse it, build list of video links
 	soup = BeautifulSoup(driver.page_source, features='html.parser')
-	video_links = [] # list to track collected links
-	videos = soup.find_all('ytd-video-renderer') # find all container boxes for videos
+	video_links = []  # list to track collected links
+	videos = soup.find_all('ytd-video-renderer')  # find all container boxes for videos
 	for i in range(len(videos)):
-		title = videos[i].find('a', id='video-title') # find link items with video title
+		title = videos[i].find('a', id='video-title')  # find link items with video title
 		video_links.append(f"https://www.youtube.com{title['href']}")
+	# print('number collected: ', len(video_links), '\n\n', video_links)
+	return video_links[:100]  # return the first 100 video links
+
+
+def imdb_search(driver: webdriver.Chrome, movie_title: str, num_results: int = 100) -> list:
+	'''
+	Scrapes videos associated with a given movie from imdb.
+
+	Args:
+		driver (webdriver.Chrome): Chrome Driver to be used
+		movie_title (str): Title of Movie
+
+	Returns:
+		list
+	'''
+	driver.get("https://www.imdb.com/")  # load page
+	driver.find_element_by_id("suggestion-search").send_keys(movie_title)  # type title
+	# driver.find_element_by_id("suggestion-search-button").click()  # click search button
+	# wait for first suggestion to load, click first one (react-autowhatever-1--item-0)
+	try:  # wait for first suggestion to load
+		wait = WebDriverWait(driver, 10)  # wait a maximum of 10 seconds for load
+		# _located() should be removed if using stable selenium
+		first_suggestion = wait.until(EC.element_to_be_clickable_located(
+			(By.ID, "react-autowhatever-1--item-0")))  # id of first item is consistent
+		first_suggestion.click() # navigate to suggested page
+	except NoSuchElementException as Error:  # page didn't load in a reasonable amount of time
+		print("\n\tError: Page didn't load in a reasonable amount of time; try again.\n")
+		print("\n\t{}\n".format(str(Error)))
+	driver.find_element_by_xpath(
+		"//span[@class='show_more quicklink']").click() # open "more" menu
+	driver.find_element_by_xpath(
+		"//a[text()='Trailers and Videos']").click() # open videos page
+	# for desired number of results, collect video links
+	video_links = []
+	while len(video_links) < num_results:
+		try: # wait for first video to be clickable as indicator of page load
+			wait = WebDriverWait(driver, 10)  # wait a maximum of 10 seconds for load
+			element = wait.until(EC.element_to_be_clickable_located(
+				(By.XPATH, "//h2/a[@class='video-modal']")))  # first video clickable
+		except NoSuchElementException as Error:  # page didn't load in a reasonable amount of time
+			print("\n\tError: Page didn't load in a reasonable amount of time; try again.\n")
+			print("\n\t{}\n".format(str(Error)))
+		# grab the source of the page and parse it, build list of video links
+		soup = BeautifulSoup(driver.page_source, features="html.parser")
+		videos = soup.find_all("a", class_="video-modal")  # find all video instances (two each vid)
+		for i in range(len(videos)):
+			if videos[i].has_attr("data-video"): # only one of two sects for each has id
+				video_links.append(
+					f"https://www.imdb.com/video/{videos[i]['data-video']}") # save link
+		# move onto next page if it exists
+		try: # try clicking the "next page" button
+			driver.find_element_by_xpath("//a[text()='Next »']").click()
+		except NoSuchElementException: # button no longer exist--must be on last page
+			break # no more results left
 	print('number collected: ', len(video_links), '\n\n', video_links)
 	return video_links[:100] # return the first 100 video links
+
 
 def search_movie(driver: webdriver.Chrome, site_name: str, movie_title: str) -> list:
 	'''
@@ -82,13 +139,14 @@ def search_movie(driver: webdriver.Chrome, site_name: str, movie_title: str) -> 
 		list
 
 	'''
-	if site_name == 'imdb':
+	if site_name == "imdb":
 		return imdb_search(driver, movie_title)
-	elif site_name == 'youtube':
+	elif site_name == "youtube":
 		return youtube_search(driver, movie_title)
 	else:
-		raise NameError('Invalid source name: use either "imdb" or "youtube"')
-	return [] # return empty list for invalid input
+		raise NameError("Invalid source name: use either 'imdb' or 'youtube'")
+	return  # return empty list for invalid input
+
 
 def main():
 	# display title
@@ -97,6 +155,7 @@ def main():
 	driver = start_driver()
 	# search for a given movie
 	search_movie(driver, "youtube", "Batman Begins")
+
 
 if __name__ == '__main__':
 	main()
